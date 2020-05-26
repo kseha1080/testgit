c521a9762aaaeccabcfbd6618267a1f1c8fc58ca
Seungha <kseha1080@gmail.com>
2020-05-26 16:02:08 -0400

    new commit 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>Testing for git2</p>
+        <p>NEW COMMIT 1</p>
         <a
           className='App-link'
           href='https://reactjs.org'
